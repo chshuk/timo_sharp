5036cf3b52c06c695767a5eb2beeeaf11c0a7492
Alexandra <chshuk@mail.ru>
2023-12-24 20:01:22 +0300

    1 lesson 3 task

---
+++ lesson_01/1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите 3 число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (max < b)
+  {max = b;}
+if (max < c)
+  { max = c;}
+Console.WriteLine(max);
