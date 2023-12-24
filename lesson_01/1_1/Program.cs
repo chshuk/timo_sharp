fe2acf8afdd37078ec71ffc3e261b16cb01d60a9
Alexandra <chshuk@mail.ru>
2023-12-24 18:46:49 +0300

    1 lesson 1 task

---
+++ lesson_01/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите 1 число: ");
+string  str1 = Console.ReadLine()!;
+Console.WriteLine("Введите 2 число: ");
+string  str2 = Console.ReadLine()!;
+
+int num1 = int.Parse(str1);
+int num2 = int.Parse(str2);
+
+Console.WriteLine("Введите 3 число: ");
+int num3 = int.Parse(Console.ReadLine()!);
